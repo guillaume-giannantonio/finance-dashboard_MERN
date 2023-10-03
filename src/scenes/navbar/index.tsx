c9450f1239b939812f117aa587268170d911d27a
guillaume-giannantonio <guillaume.giannantonio@protonmail.com>
2023-10-03 13:50:46 +0200

    add dashboard basis

---
+++ src/scenes/navbar/index.tsx
@@ -0,0 +1,51 @@
+import { Box, Typography, useTheme } from '@mui/material';
+import FlexBetween from '@/components/FlexBetween.tsx';
+import PixIcon from '@mui/icons-material/Pix';
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
+
+function Navbar() {
+  const { palette } = useTheme();
+  const [selected, setSelected] = useState('dashboard');
+  return (
+    <FlexBetween mb="0.25rem" p="0.5rem 0rem" color={palette.grey[300]}>
+      {/* Left Side*/}
+      <FlexBetween gap="0.75rem">
+        <PixIcon sx={{ fontSize: '28px' }} />
+        <Typography variant="h4" fontSize="16px">
+          Finanseer
+        </Typography>
+      </FlexBetween>
+
+      <FlexBetween gap="2rem">
+        <Box sx={{ '&:hover': { color: palette.primary[100] } }}>
+          <Link
+            to="/dashboard"
+            onClick={() => setSelected('dashboard')}
+            style={{
+              color: selected === 'dashboard' ? 'inherit' : palette.grey[700],
+              textDecoration: 'inherit',
+            }}
+          >
+            dashboard
+          </Link>
+        </Box>
+        <Box sx={{ '&:hover': { color: palette.primary[100] } }}>
+          <Link
+            to="/predictions"
+            onClick={() => setSelected('predictions')}
+            style={{
+              color: selected === 'predictions' ? 'inherit' : palette.grey[700],
+              textDecoration: 'inherit',
+            }}
+          >
+            predictions
+          </Link>
+        </Box>
+      </FlexBetween>
+      {/* Right Side*/}
+    </FlexBetween>
+  );
+}
+
+export default Navbar;
